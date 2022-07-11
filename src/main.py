8a9a0c274ca77752326462fa9c7ac6ef80e95971
Dan Nicholson <dbn@endlessos.org>
2022-07-11 13:09:42 -0600

    Raise log level to DEBUG
    Except for pyjnius, which is too noisy.

--- src/main.py
+++ src/main.py
@@ -2,5 +2,6 @@ from browser import app
 import logging
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('jnius').setLevel(logging.INFO)
 app.run(debug=False)
